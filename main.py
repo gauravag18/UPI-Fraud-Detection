97622c47678780e4ea4ad0129bf6a129417b4411
Gaurav Agarwalla <agarwalgaurav1809@gmail.com>
2026-04-02 01:36:14 +0530

    added gitignore

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.transaction.preprocess import preprocess_pipeline
+
+X, y = preprocess_pipeline("transaction_data/upi_transactions_2024.csv")
+
+print(X.shape)
+print(y.value_counts())
---
+++ src/transaction/preprocess.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+
+def load_data(path):
+    df = pd.read_csv(path)
+    return df
+
+def basic_cleaning(df):
+    df = df.copy()
+    df = df.drop(columns=["transaction_id"], errors="ignore")
+    df = df.dropna()
+    return df
+
+def encode_categorical(df):
+    df = df.copy()
+
+    categorical_cols = [
+        "transaction_type",
+        "merchant_category",
+        "sender_age_group",
+        "receiver_age_group",
+        "sender_state",
+        "sender_bank",
+        "receiver_bank",
+        "device_type",
+        "network_type",
+        "day_of_week",
+        "transaction_status"
+    ]
+
+    df = pd.get_dummies(df, columns=categorical_cols, drop_first=True)
+
+    return df
+
+def process_time(df):
+    df = df.copy()
+
+    if "timestamp" in df.columns:
+        df["timestamp"] = pd.to_datetime(df["timestamp"])
+        df["hour"] = df["timestamp"].dt.hour
+        df["day"] = df["timestamp"].dt.day
+        df = df.drop(columns=["timestamp"])
+
+    return df
+
+def split_features_target(df):
+    df = df.copy()
+
+    y = df["fraud_flag"]
+    X = df.drop(columns=["fraud_flag"])
+
+    return X, y
+
+def preprocess_pipeline(path):
+    df = load_data(path)
+    df = basic_cleaning(df)
+    df = process_time(df)
+    df = encode_categorical(df)
+    X, y = split_features_target(df)
+    return X, y
