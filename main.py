2050b270cf41dce31d6b60e7f31503748714262c
Gaurav Agarwalla <agarwalgaurav1809@gmail.com>
2026-04-02 12:54:00 +0530

    added baseline model

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from src.transaction.preprocess import preprocess_pipeline
+from src.transaction.train_logistic import train_logistic
 
 X, y = preprocess_pipeline("transaction_data/upi_transactions_2024.csv")
 
-print(X.shape)
-print(y.value_counts())
-print("Fraud %:", (y.sum() / len(y)) * 100)
+model, X_test, y_test = train_logistic(X, y)
